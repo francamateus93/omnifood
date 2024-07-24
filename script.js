5ec217231344ac67a4c0d588acab3795052da02e
francamateus93 <francamateus@gmail.com>
2024-07-24 19:50:14 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const myName = "Omnifood";
+const h1 = document.querySelector(".heading-primary");
+console.log("myName");
+console.log("h1");
+
+// h1.addEventListener("click", function () {
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
+
+const yearEL = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+console.log("currentYear");
+yearEL.textContent = currentYear;

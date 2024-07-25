e4b6bb4aa5521810bd01658f228ffe9f8599f411
francamateus93 <francamateus@gmail.com>
2024-07-25 17:32:39 +0200

    tailwind1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./input.css", "./script.js"],
   theme: {
     extend: {},
   },
